93d02b5fa3125e9d8662f335e919de4faa2a4616
Bit Byte Labs <bitbytelabs@outlook.com>
2026-02-20 18:54:23 +1100

    fix: make workflow engine paths robust and validated

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -1,13 +1,13 @@
 /*
-  Stockfish, a UCI chess playing engine derived from Glaurung 2.1
-  Copyright (C) 2004-2026 The Stockfish developers (see AUTHORS file)
+  GlowingJellyfish, a UCI chess playing engine derived from Glaurung 2.1
+  Copyright (C) 2004-2026 The GlowingJellyfish developers (see AUTHORS file)
 
-  Stockfish is free software: you can redistribute it and/or modify
+  GlowingJellyfish is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  Stockfish is distributed in the hope that it will be useful,
+  GlowingJellyfish is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
@@ -30,7 +30,7 @@
     #include <immintrin.h>
 #endif
 
-namespace Stockfish {
+namespace GlowingJellyfish {
 
 namespace {
 
@@ -309,4 +309,4 @@ Move* generate<LEGAL>(const Position& pos, Move* moveList) {
     return moveList;
 }
 
-}  // namespace Stockfish
+}  // namespace GlowingJellyfish
--- src/thread_win32_osx.h
+++ src/thread_win32_osx.h
@@ -1,13 +1,13 @@
 /*
-  Stockfish, a UCI chess playing engine derived from Glaurung 2.1
-  Copyright (C) 2004-2026 The Stockfish developers (see AUTHORS file)
+  GlowingJellyfish, a UCI chess playing engine derived from Glaurung 2.1
+  Copyright (C) 2004-2026 The GlowingJellyfish developers (see AUTHORS file)
 
-  Stockfish is free software: you can redistribute it and/or modify
+  GlowingJellyfish is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  Stockfish is distributed in the hope that it will be useful,
+  GlowingJellyfish is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
@@ -32,7 +32,7 @@
     #include <pthread.h>
     #include <functional>
 
-namespace Stockfish {
+namespace GlowingJellyfish {
 
 class NativeThread {
     pthread_t thread;
@@ -63,15 +63,15 @@ class NativeThread {
     void join() { pthread_join(thread, nullptr); }
 };
 
-}  // namespace Stockfish
+}  // namespace GlowingJellyfish
 
 #else  // Default case: use STL classes
 
-namespace Stockfish {
+namespace GlowingJellyfish {
 
 using NativeThread = std::thread;
 
-}  // namespace Stockfish
+}  // namespace GlowingJellyfish
 
 #endif
 
